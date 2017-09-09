7fbf37a878e0b89729a2d900dd98db28e5d58c65
0000sir <0000sir@gmail.com>
2017-09-09 10:45:13 +0800

    first workable version, but AQI api returns 500 error

--- html/aqi.js
+++ html/aqi.js
@@ -1,3 +1,14 @@
+/*
+AQI standers
+
+0-50
+51-100
+101-150
+151-200
+201-300
+>300
+*/
+
 function read_aqi(city){
   $.get('/api/querys/only_aqi.json?token=5j1znBVAsnSf5xQyNQyq&city='+ city, function(data){
     avg_aqi = data[data.length-1];
@@ -5,19 +16,33 @@ function read_aqi(city){
     $("#aqi .quality").text(avg_aqi.quality);
     $("#aqi .value").text(avg_aqi.aqi);
     $("#aqi .time").text(avg_aqi.time_point);
+
+    if(avg_aqi.aqi<51){
+      $('body').attr("className", "aqi_good");
+    }else if(avg_aqi.aqi < 101){
+      $('body').attr("className", "aqi_normal");
+    }else if(avg_aqi.aqi < 151){
+      $('body').attr("className", "aqi_bad");
+    }else if(avg_aqi.aqi < 201){
+      $('body').attr("className", "aqi_harm");
+    }else{
+      $('body').attr("className", "aqi_hell");
+    }
   });
+
 }
 
 function read_weather(city){
   var weather;
   $.get('/weather/?app=weather.today&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json&weaid='+city, function(data){
     weather = data.result;
-    $('#weather .weather').append( "<img src="+ weather.weather_icon +">" );
-    $('#weather .weather').text(weather.weather_curr);
-    $('#weather .wind').text( weather.wind + weather.winp );
-    $('#weather .temperature').text(weather.temperature_curr);
-    $('#weather .weather_scope').text(weather.weather);
-    $('#weather .temperature_scope').text(weather.temperature);
+    $('#date').text(weather.days + "  " + weather.week)
+    $('.weather').append( "<img src="+ weather.weather_icon +">" );
+    $('.weather').text(weather.weather_curr);
+    $('.wind').text( weather.wind + weather.winp );
+    $('.temperature').text(weather.temperature_curr);
+    $('.weather_scope').text(weather.weather);
+    $('.temperature_scope').text(weather.temperature);
 
   });
   return weather;
