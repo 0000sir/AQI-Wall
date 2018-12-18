bd30fef58f8729736ca395beb999eec5c7aec695
0000 <fenglu@nacta.edu.cn>
2018-12-18 15:32:25 +0800

    omit empty value

--- html/api.js
+++ html/api.js
@@ -13,33 +13,16 @@ function read_aqi(city){
   $.get('/aqi/'+ city+'/?token=01e7a36f4fc582977c68e8fde4429880f40fc98e', function(data){
     avg_aqi = 0;
     avg_aqi = data['data']['aqi'];
-    console.log(avg_aqi);
 
-    $("#aqi .value").text(avg_aqi);
-    $("#aqi .time").text(data['data']['time']['s'].substring(11,19) + " 更新");
+    updated_at = data['data']['time']['s'].substring(11,19);
+
     city = data['data']['city']['name'];
     city_name_start = city.indexOf('\(');
-    $("#aqi .location").text(city.substring(city_name_start+1, city.length-1));
-    if(avg_aqi<51){
-      $('body').attr("class", "aqi_good");
-      $("#aqi .quality").text("优");
-    }else if(avg_aqi < 101){
-      $('body').attr("class", "aqi_normal");
-      $("#aqi .quality").text("良");
-    }else if(avg_aqi < 151){
-      $('body').attr("class", "aqi_bad");
-      $("#aqi .quality").text("轻度污染");
-    }else if(avg_aqi < 201){
-      $('body').attr("class", "aqi_harm");
-      $("#aqi .quality").text("中度污染");
-    }else if(avg_aqi < 301){
-      $('body').attr("class", "aqi_harm");
-      $("#aqi .quality").text("重度污染");
-    }else{
-      $('body').attr("class", "aqi_hell");
-      $("#aqi .quality").text("有毒");
-    }
-    
+    city_name = city.substring(city_name_start+1, city.length-1);
+
+    source = data['data']['attributions'][0]['name'];
+    source = source.substring(source.indexOf('\(')+1, source.length-1);
+    show_aqi(avg_aqi, updated_at, city_name, source);
   });
 
 }
@@ -48,12 +31,11 @@ function read_weather(city){
   var weather;
   $.get('/weather?key=a2cefdbe177a4dfd961eb841b66d667c&location='+city, function(data){
     weather = data.HeWeather6[0];
-    console.log(weather);
     $('.weather').text(weather.now.cond_txt);
     $('.wind').text( weather.now.wind_dir + weather.now.wind_sc + '级' );
     $('.temperature').text(weather.now.tmp+'℃');
     $('.humidity').text("相对湿度"+weather.now.hum+"%");
-    
+
     forcast = weather.daily_forecast;
     $('#future').text('');
     for(var day=0; day<forcast.length;day++){
@@ -66,6 +48,36 @@ function read_weather(city){
 
   });
   return weather;
+}
+
+function show_aqi(aqi, updated_at, city){
+  $("#aqi .value").text(aqi);
+  if(aqi == '-'){
+    $('body').attr("class", "aqi_normal");
+    $("#aqi .quality").text("暂无数据");
+  }else if(aqi<51){
+    $('body').attr("class", "aqi_good");
+    $("#aqi .quality").text("优");
+  }else if(aqi < 101){
+    $('body').attr("class", "aqi_normal");
+    $("#aqi .quality").text("良");
+  }else if(aqi < 151){
+    $('body').attr("class", "aqi_bad");
+    $("#aqi .quality").text("轻度污染");
+  }else if(aqi < 201){
+    $('body').attr("class", "aqi_harm");
+    $("#aqi .quality").text("中度污染");
+  }else if(aqi < 301){
+    $('body').attr("class", "aqi_harm");
+    $("#aqi .quality").text("重度污染");
+  }else{
+    $('body').attr("class", "aqi_hell");
+    $("#aqi .quality").text("有毒");
+  }
+
+  $("#aqi .time").text(updated_at + " 更新");
+  $("#aqi .location").text(city);
+  $("#aqi .source").text(source);
 }
 
 function clock(){
