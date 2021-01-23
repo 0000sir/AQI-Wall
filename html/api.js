b14c61d678ea9aa357ed5513546a87d317116f96
0000 <fenglu@nacta.edu.cn>
2021-01-23 11:10:30 +0800

    style: 连续变化的背景色

--- html/api.js
+++ html/api.js
@@ -22,9 +22,9 @@ function read_aqi(city){
 
     source = data['data']['attributions'][0]['name'];
     source = source.substring(source.indexOf('\(')+1, source.length-1);
+    set_bg(aqi_color(avg_aqi));
     show_aqi(avg_aqi, updated_at, city_name, source);
   });
-
 }
 
 function read_weather(city){
@@ -41,7 +41,7 @@ function read_weather(city){
     for(var day=0; day<forcast.length;day++){
       cast = forcast[day];
       $('#future').append("<li><div class=\"date\">"+cast.date+
-        "</div><div class=\"temperature\">"+cast.tmp_min+'-'+cast.tmp_max+
+        "</div><div class=\"temperature\">"+cast.tmp_min+'~'+cast.tmp_max+"℃"+
         "</div><div class=\"weather\">"+cast.cond_txt_d+'-'+cast.cond_txt_n+
         "</div><div class=\"windp\">"+cast.wind_dir+cast.wind_sc+"</div></li>");
     }
@@ -54,24 +54,18 @@ function show_aqi(aqi, updated_at, city){
   $("#aqi .value").text(aqi);
   if(aqi == '-'){
     $('body').attr("class", "aqi_normal");
-    $("#aqi .quality").text("暂无数据");
+    $("#aqi .quality").text("无数据");
   }else if(aqi<51){
-    $('body').attr("class", "aqi_good");
     $("#aqi .quality").text("优");
   }else if(aqi < 101){
-    $('body').attr("class", "aqi_normal");
     $("#aqi .quality").text("良");
   }else if(aqi < 151){
-    $('body').attr("class", "aqi_bad");
     $("#aqi .quality").text("轻度污染");
   }else if(aqi < 201){
-    $('body').attr("class", "aqi_harm");
     $("#aqi .quality").text("中度污染");
   }else if(aqi < 301){
-    $('body').attr("class", "aqi_harm");
     $("#aqi .quality").text("重度污染");
   }else{
-    $('body').attr("class", "aqi_hell");
     $("#aqi .quality").text("有毒");
   }
 
@@ -113,4 +107,34 @@ function clock(){
   }
   time=y+'-'+m+'-'+d+' '+fa(h)+':'+fa(f);
   $('#date').text(time);
+}
+
+function aqi_color(aqi){
+  var r = 0;
+  var g = 255;
+  var b = 0;
+
+  if(aqi=="-"){
+    return [204,204,150];
+  }
+  
+  g = (0.99**aqi)*255;
+  if(aqi<=250){
+    r = (aqi**0.25)*50;
+    b = 0;
+  }else{
+    r = (0.99**(aqi-250))*250
+    b = (0.995**(aqi-250))*250
+  }
+  return [r,g,b];
+}
+
+function set_bg(arr_r_g_b){
+  $("body").css("background-color","rgb("+arr_r_g_b.join(",")+")");
+}
+
+function color_test(){
+  for(i=1;i<600;i++){
+    $("body").append("<div style='width:100px;height:5px;background-color:rgb("+aqi_color(i).join(",")+");'></div>");
+  }
 }
